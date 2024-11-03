a447b3994bb015a112e9472bdfd959c59e811654
kitae0522 <kitae040522@gmail.com>
2024-11-03 15:13:38 +0900

    FIX: GenerateErrorCtx 함수 리턴 타입

--- pkg/exception/error.go
+++ pkg/exception/error.go
@@ -16,8 +16,8 @@ func CreateErrorResponse(ctx *fiber.Ctx, statusCode int, errMessage string, err 
 	return ctx.Status(statusCode).JSON(ctxError)
 }
 
-func GenerateErrorCtx(statusCode int, errMessage string, err interface{}) ErrResponseCtx {
-	return ErrResponseCtx{
+func GenerateErrorCtx(statusCode int, errMessage string, err interface{}) *ErrResponseCtx {
+	return &ErrResponseCtx{
 		IsError:    true,
 		StatusCode: statusCode,
 		Message:    errMessage,
